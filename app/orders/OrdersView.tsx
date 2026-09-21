7de9e9104e550c0a9548d475509e03a5d207bf1b
ballinghockey <secure@ballinghockey.com>
2026-09-21 08:54:46 -0300

    Update OrdersView.tsx

--- app/orders/OrdersView.tsx
+++ app/orders/OrdersView.tsx
@@ -181,7 +181,6 @@ export default function OrdersView({
                           </td>
                         </tr>
                       )}
-                      )}
                         {order.vat_total > 0 && (
                           <tr>
                             <td colSpan={3} className="pt-1 text-right text-xs text-neutral-400">
